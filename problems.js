319a053a52af8abda5edbd5396a621fec523b973
worldofcreatives <worldofcreatives.com@gmail.com>
2023-10-01 18:02:49 -0400

    Update problems.js

--- problems.js
+++ problems.js
@@ -36,7 +36,7 @@ function wordWithinArray(word, arr) {
 };
 
 function echo(str) {
-    /* Takes in a string and returns that string "echo-ized". E.g. 
+    /* Takes in a string and returns that string "echo-ized". E.g.
     echo("Mom!"); // => returns "MOM! ... Mom! ... mom!"
     echo("hey"); // => returns "HEY ... hey ... hey"
     echo("JUMp"); // => returns "JUMP ... JUMp ... jump" */
@@ -84,7 +84,7 @@ function aCounter(word) {
     function to use a `for` loop instead of the `while` loop it is currently
     using. */
 
-    /* 
+    /*
     let index = 0;
     let count = 0;
     while (index < word.length) {
@@ -113,5 +113,5 @@ module.exports = {
     isFive,
     isOdd,
     isSubString,
-    aCounter   
+    aCounter
 }
